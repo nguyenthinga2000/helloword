3958b4203bb138072ff811f89e134a94dacafaf6
nguyenthinga2000 <77582008+nguyenthinga2000@users.noreply.github.com>
2021-01-17 23:26:37 +0700

    Add files via upload

---
+++ Baitap/HelloWord/HelloWord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Nhap Ten Nguoi Dung");
+            Console.WriteLine("Chao mung:" + Console.ReadLine() + " den voi mon hoc LTQL");// in ra man hinh
+            Console.WriteLine("Giai phuong trinh bac nhat ax + b =0:");
+            Console.WriteLine("Nhap he so a:");
+            string temp1 = Console.ReadLine();
+            int a = int.Parse(temp1);
+            Console.WriteLine("Nhap he so b:");
+            string temp2 = Console.ReadLine();
+            int b = int.Parse(temp2);
+            int x = -b / a;
+            Console.WriteLine("Phuong trinh:" + a + "x +" + b + " =0 co nghiem la: x = " + x);
+        }
+    }
+}
